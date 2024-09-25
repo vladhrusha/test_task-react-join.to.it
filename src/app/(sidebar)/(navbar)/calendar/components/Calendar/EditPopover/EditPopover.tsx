573b9342d151b2ea41d6481a8cf9d7301372c298
vladhrusha <hrushavladyslavwork@gmail.com>
2024-09-25 21:22:14 +0300

    more styling updates

--- src/app/(sidebar)/(navbar)/calendar/components/Calendar/EditPopover/EditPopover.tsx
+++ src/app/(sidebar)/(navbar)/calendar/components/Calendar/EditPopover/EditPopover.tsx
@@ -4,6 +4,7 @@ import { Button, Popover, Stack, TextField } from "@mui/material";
 import { DesktopDatePicker, DesktopTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 import React from "react";
+import * as colors from "@/styles/colors";
 
 const EditPopover = ({ modalInfo, formData, handleClose, setFormData, handleEdit, handleDelete }) => {
   return (
@@ -18,36 +19,42 @@ const EditPopover = ({ modalInfo, formData, handleClose, setFormData, handleEdit
         left: modalInfo.left,
         backgroundColor: "white",
         width: "230px",
-        height: "300px",
+        height: "320px",
       }}
     >
       <Stack
         sx={{
           width: "201px",
-          height: "265px",
-          padding: "7px",
+          height: "275px",
+          padding: "16px",
         }}
       >
-        <FontAwesomeIcon icon={faCircleXmark} onClick={handleClose} />
+        <FontAwesomeIcon
+          icon={faCircleXmark}
+          onClick={handleClose}
+          style={{ color: colors.primary.icons, alignSelf: "end" }}
+        />
         <TextField
           label="event name"
           variant="standard"
           size="small"
-          maxLength={30}
           value={formData.title}
           onChange={(event) => setFormData((prev) => ({ ...prev, title: event.target.value }))}
         />
 
         <LocalizationProvider dateAdapter={AdapterMoment}>
-          <DesktopTimePicker
-            value={formData.time}
-            onChange={(value) => setFormData((prev) => ({ ...prev, time: value }))}
-          />
-        </LocalizationProvider>
-        <LocalizationProvider dateAdapter={AdapterMoment}>
           <DesktopDatePicker
             value={formData.date}
+            slotProps={{ textField: { variant: "standard", size: "medium" } }}
             onChange={(value) => setFormData((prev) => ({ ...prev, date: value }))}
+          />
+        </LocalizationProvider>
+
+        <LocalizationProvider dateAdapter={AdapterMoment}>
+          <DesktopTimePicker
+            value={formData.time}
+            slotProps={{ textField: { variant: "standard" } }}
+            onChange={(value) => setFormData((prev) => ({ ...prev, time: value }))}
           />
         </LocalizationProvider>
 
@@ -59,12 +66,16 @@ const EditPopover = ({ modalInfo, formData, handleClose, setFormData, handleEdit
           onChange={(event) => setFormData((prev) => ({ ...prev, description: event.target.value }))}
         />
 
-        <Stack flexDirection="row" justifyContent="space-around">
-          <Button variant="text" color="primary" onClick={handleDelete}>
-            Discard
+        <Stack flexDirection="row" justifyContent="space-between" paddingTop="50px">
+          <Button
+            variant="text"
+            sx={{ color: colors.status.error, textTransform: "none" }}
+            onClick={handleDelete}
+          >
+            Cancel
           </Button>
-          <Button variant="text" color="primary" onClick={handleEdit}>
-            Edit
+          <Button variant="text" sx={{ textTransform: "none" }} color="inherit" onClick={handleEdit}>
+            Save
           </Button>
         </Stack>
       </Stack>
