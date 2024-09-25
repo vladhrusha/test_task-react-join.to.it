0f38f9df9fec977554d3f3b6a6ae499ffcc38a59
vladhrusha <hrushavladyslavwork@gmail.com>
2024-09-25 22:36:46 +0300

    vercel try fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/test_task-react-join.to.it",
-  output: "export",
-  reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  //   basePath: "/test_task-react-join.to.it",
+  //   output: "export",
+  //   reactStrictMode: true,
+  //   typescript: {
+  //     ignoreBuildErrors: true,
+  //   },
+  output: "standalone",
 };
 
 export default nextConfig;
